5e275796304556c1a01c681212c582e67c18f393
ActivexDiamond <activexdiamond@gmail.com>
2025-03-19 19:47:42 +0300

    test: misc. tests for Window.eventSystem

--- src/lve/init.lua
+++ src/lve/init.lua
@@ -1,12 +1,36 @@
 local glfw = require "moonglfw"
+local WINDOW = require "lve.globals.WINDOW"
 
-lvr = {}
-lvr.window = glfw.create_window(800, 600, "OpenLVR")
+local EvKeyboard = require "lve.event.keyboard.EvKeyboard"
+local EvKeyPress = require "lve.event.keyboard.EvKeyPress"
+local EvKeyRelease = require "lve.event.keyboard.EvKeyRelease"
 
-require "lve.initEvents"
+local curEv = require "lve.event.Event"
 
-while not glfw.window_should_close(lvr.window) do
-	glfw.poll_events()
-	-- lvr.events.poll()
-end
+WINDOW.eventSystem:attach({}, curEv, function(_, e)
+	for k, v in pairs(e) do print(k, v) end
+end)
 
+WINDOW.eventSystem:attach({}, EvKeyPress, function(_, e)
+	if e.key == 'escape' then glfw.set_window_should_close(WINDOW.window, true) end
+end)
+
+local gl = require "moongl"
+glfw.make_context_current(WINDOW.window)
+-- gl.init()
+
+print(glfw.get_current_context())
+WINDOW:run()
+
+a = EvKeyPress()
+b = EvKeyPress()
+
+print(b)
+
+local c = 3
+
+c = 1900
+
+c = 2
+
+print(c)
